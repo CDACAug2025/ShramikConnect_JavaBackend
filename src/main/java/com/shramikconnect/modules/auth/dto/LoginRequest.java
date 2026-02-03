443ddc38016f9c0f49e3b4b3ebd3552f1cc3df77
Dhirajd1310 <dp370568@gmail.com>
2026-02-03 10:58:56 +0530

    added validation

--- src/main/java/com/shramikconnect/modules/auth/dto/LoginRequest.java
+++ src/main/java/com/shramikconnect/modules/auth/dto/LoginRequest.java
@@ -1,13 +1,22 @@
 package com.shramikconnect.modules.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import lombok.*;
-
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
 
-    private String username;
+	@NotBlank(message = "Email/Username is required")
+	@Email(message = "Please provide a valid email address") 
+	private String username;
+
+	@NotBlank(message = "Password is required")
     private String password;
 }
-
--- src/main/java/com/shramikconnect/modules/auth/dto/ResetPasswordRequest.java
+++ src/main/java/com/shramikconnect/modules/auth/dto/ResetPasswordRequest.java
@@ -1,10 +1,26 @@
 package com.shramikconnect.modules.auth.dto;
 
-import lombok.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class ResetPasswordRequest {
+    
+    @NotBlank(message = "Reset token is missing")
     private String token;
+
+    @NotBlank(message = "Password is required")
+    @Pattern(
+        regexp = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!]).*$",
+        message = "Invalid password format"
+    )
     private String newPassword;
+
+    @NotBlank(message = "Confirm password is required")
     private String confirmPassword;
 }
